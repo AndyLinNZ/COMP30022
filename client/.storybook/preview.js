f57784e7fd4fb88b4c87714f998f5a41f2a7c3a1
JoshDubar <joshuadubar123@gmail.com>
2021-08-21 17:12:38 +1000

    moved theme to own file

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,35 +1,11 @@
-import { ChakraProvider, extendTheme, CSSReset } from '@chakra-ui/react'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
 import React from 'react'
+import { theme } from '../theme'
 
 export const parameters = {
     controls: { expanded: true },
     layout: 'centered',
 }
-
-const theme = extendTheme({
-    fonts: {
-        body: 'Roboto Condensed',
-        heading: 'Roboto Condensed',
-    },
-    colors: {
-        grey: '#EBEBEB',
-        white: '#fff',
-        activeSeasonBg: '#FFF4DD',
-        inactiveSeasonBg: '#707070',
-        activeSeasonColor: '#F57405',
-        inactiveSeasonColor: 'white',
-        // had to add 500 for checkbox
-        greyText: {
-            500: '#3E3D3D',
-        },
-        black: {
-            500: '#000',
-        },
-        orange: '#FFBD2D',
-        heading: '#15110D',
-    },
-    breakpoints: ['600px', '1024px', '1440px'],
-})
 
 const withChakra = (storyFn) => (
     <ChakraProvider theme={theme}>
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,32 +1,8 @@
 import React from 'react'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 import '@fontsource/roboto-condensed'
-
-const theme = extendTheme({
-    fonts: {
-        body: 'Roboto Condensed',
-        heading: 'Roboto Condensed',
-    },
-    colors: {
-        grey: '#EBEBEB',
-        white: '#fff',
-        activeSeasonBg: '#FFF4DD',
-        inactiveSeasonBg: '#707070',
-        activeSeasonColor: '#F57405',
-        inactiveSeasonColor: 'white',
-        // had to add 500 for checkbox
-        greyText: {
-            500: '#3E3D3D',
-        },
-        black: {
-            500: '#000',
-        },
-        orange: '#FFBD2D',
-        heading: '#15110D',
-    },
-    breakpoints: ['600px', '1024px', '1440px'],
-})
+import { theme } from '../theme'
 
 function MyApp({ Component, pageProps }) {
     return (
