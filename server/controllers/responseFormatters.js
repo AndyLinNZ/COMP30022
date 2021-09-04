701489f261c3ff7342515a83236e69409272f612
Joseph Surin <joseph.surin@gmail.com>
2021-09-04 14:17:30 +1000

    include _id in response

--- server/controllers/responseFormatters.js
+++ server/controllers/responseFormatters.js
@@ -1,11 +1,11 @@
 const { pick } = require('./util')
 
 const formatLeagueResp = (leagueDoc) =>
-    pick(leagueDoc, ['name', 'organisation', 'creator', 'admins', 'seasons'])
+    pick(leagueDoc, ['_id', 'name', 'organisation', 'creator', 'admins', 'seasons'])
 
 const formatUserResp = (userDoc) => {
     var leagues = userDoc.leagues.map(formatLeagueResp)
-    var details = pick(userDoc, ['email', 'firstName', 'lastName'])
+    var details = pick(userDoc, ['_id', 'email', 'firstName', 'lastName'])
     return { ...details, leagues }
 }
 
