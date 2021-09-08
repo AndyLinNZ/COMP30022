c63d66676723e64af835842a7653776100679698
Joseph Surin <joseph.surin@gmail.com>
2021-09-08 23:54:37 +1000

    remove gameResult router

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,6 +7,7 @@ const seasonRouter = require('./seasonRouter')
 const gradeRouter = require('./gradeRouter')
 const teamRouter = require('./teamRouter')
 const playerRouter = require('./playerRouter')
+const gameRouter = require('./gameRouter')
 
 // routes
 router.use('/auth', authRouter)
@@ -16,6 +17,7 @@ router.use('/season', seasonRouter)
 router.use('/grade', gradeRouter)
 router.use('/team', teamRouter)
 router.use('/player', playerRouter)
+router.use('/game', gameRouter)
 
 // general error handling
 router.use((err, req, res, _) => {
