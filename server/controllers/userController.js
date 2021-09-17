140c65bfa71fe9efb68ea4c94eb6af39682722ef
Joseph Surin <joseph.surin@gmail.com>
2021-09-17 15:40:05 +1000

    populate games in get user details

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,13 @@ const { formatUserResp } = require('./responseFormatters')
 
 async function getUserDetails(req, res, next) {
     var user = await req.user.execPopulate('leagues')
-    user = await req.user.execPopulate('teams')
+    user = await req.user.execPopulate({
+        path: 'teams',
+        populate: {
+            path: 'games',
+            model: 'Game',
+        },
+    })
 
     return res.status(200).json({
         success: true,
