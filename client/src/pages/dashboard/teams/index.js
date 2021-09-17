89274c9ae9754948c0a32d87a09744e084b45666
Joseph Surin <joseph.surin@gmail.com>
2021-09-17 14:03:03 +1000

    display user teams instead of mock teams

--- client/src/pages/dashboard/teams/index.js
+++ client/src/pages/dashboard/teams/index.js
@@ -3,12 +3,6 @@ import { useUserDetails } from 'hooks'
 import { Template, Container, Capsule, CreateCapsule } from 'components/Dashboard'
 import { VStack } from '@chakra-ui/react'
 
-const mockTeams = [
-    { name: 'Lygon Kangaroos' },
-    { name: 'Authentication Geniuses' },
-    { name: 'Roaring Rhinosaurs' },
-    // { name: 'Sprinting Sandals' },
-]
 const index = () => {
     const { user } = useUserDetails()
 
@@ -18,10 +12,10 @@ const index = () => {
         <Template>
             <Container heading={heading}>
                 <VStack spacing="1.25rem">
-                    {mockTeams.map((team) => {
+                    {user?.teams?.map((team) => {
                         return <Capsule key={team._id} name={team.name} path={team._id} />
                     })}
-                    {mockTeams.length < 4 && <CreateCapsule heading="CREATE A NEW TEAM" />}
+                    {user?.teams?.length < 4 && <CreateCapsule heading="CREATE A NEW TEAM" />}
                 </VStack>
             </Container>
         </Template>
