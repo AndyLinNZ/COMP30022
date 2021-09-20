4236bf8b3a72bafee39b16980540fb16dad1f62c
Andy Lin <andylinnz79@gmail.com>
2021-09-20 16:16:01 +1000

    update edit seasons page to include delete confirmation and to utilise deleteSeason

---
+++ client/src/components/Form/DeleteConfirm.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+} from '@chakra-ui/react'
+import { FormButton } from 'components/Form'
+
+const DeleteConfirm = ({ isOpen, onClose, onDelete, toDeleteText = '' }) => {
+    const cancelRef = React.useRef()
+    const { mutate: deleteFn, isLoading, isSuccess } = onDelete
+    return (
+        <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+            <AlertDialogOverlay>
+                <AlertDialogContent>
+                    <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                        Delete {toDeleteText}
+                    </AlertDialogHeader>
+
+                    <AlertDialogBody>
+                        Are you sure? You can't undo this action afterwards.
+                    </AlertDialogBody>
+
+                    <AlertDialogFooter spacing="0.5rem">
+                        <FormButton onClick={onClose}>Cancel</FormButton>
+                        <FormButton
+                            bg="red"
+                            onClick={() => deleteFn()}
+                            isLoading={isLoading || isSuccess}
+                            ml={3}
+                        >
+                            Confirm
+                        </FormButton>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialogOverlay>
+        </AlertDialog>
+    )
+}
+
+export default DeleteConfirm
