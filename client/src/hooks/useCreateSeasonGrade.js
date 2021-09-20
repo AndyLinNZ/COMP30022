4657e9785e258ae627953058eecb6805e9c86310
Joseph Surin <joseph.surin@gmail.com>
2021-09-20 12:29:39 +1000

    fix create season grade and get all season grade api/hooks

--- client/src/hooks/useCreateSeasonGrade.js
+++ client/src/hooks/useCreateSeasonGrade.js
@@ -1,13 +1,12 @@
 import { useMutation } from 'react-query'
 import { createSeasonGrade } from 'api'
 import useUserDetails from './useUserDetails'
-import { getSeasonFromUser, getLeagueFromUser } from 'utils'
+import { getSeasonFromUser } from 'utils'
 
 const useCreateSeasonGrade = (options = {}) => {
     const { user } = useUserDetails()
-    const leagueId = getLeagueFromUser(user)?._id
     const seasonId = getSeasonFromUser(user)?._id
-    return useMutation((data) => createSeasonGrade(data, seasonId, leagueId), options)
+    return useMutation((data) => createSeasonGrade(data, seasonId), options)
 }
 
 export default useCreateSeasonGrade
--- client/src/hooks/useGetAllSeasonGrades.js
+++ client/src/hooks/useGetAllSeasonGrades.js
@@ -1,13 +1,12 @@
 import { useQuery } from 'react-query'
 import { getAllSeasonGrades } from 'api'
-import { extractData, getSeasonFromUser, getLeagueFromUser } from 'utils'
+import { extractData, getSeasonFromUser } from 'utils'
 import useUserDetails from './useUserDetails'
 
 const useGetAllSeasonGrades = (options = {}) => {
     const { user } = useUserDetails()
-    const leagueId = getLeagueFromUser(user)?._id
     const seasonId = getSeasonFromUser(user)?._id
-    const { data, isLoading, error } = useQuery(['grades', seasonId, leagueId], getAllSeasonGrades, options)
+    const { data, isLoading, error } = useQuery(['grades', seasonId], getAllSeasonGrades, options)
     const grades = extractData(data)
     return { grades, isLoading, error }
 }
