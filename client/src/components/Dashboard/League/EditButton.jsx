88305d0866a99703fb1c7e7975d4bd1f1b203bfd
Andy Lin <andylinnz79@gmail.com>
2021-10-10 22:00:02 +1100

    replace error icons

--- client/src/components/Dashboard/League/EditButton.jsx
+++ client/src/components/Dashboard/League/EditButton.jsx
@@ -3,16 +3,16 @@ import { IconButton } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import React from 'react'
 
-const EditButton = ({ name }) => {
+const EditButton = ({ name, icon = <EditIcon />, path = 'edit' }) => {
     const router = useRouter()
     return (
         <IconButton
-            icon={<EditIcon />}
+            icon={icon}
             size="lg"
             alignSelf="center"
             justifySelf="center"
             cursor="pointer"
-            onClick={() => router.push(`${window.location.pathname}/${name}/edit`)}
+            onClick={() => router.push(`${window.location.pathname}/${name}/${path}`)}
             marginLeft="1rem"
         />
     )
--- client/src/components/RoundsView.jsx
+++ client/src/components/RoundsView.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-import { Tabs, TabList, TabPanels, Tab, TabPanel, Text, Center, VStack, HStack } from "@chakra-ui/react"
+import {
+    Tabs,
+    TabList,
+    TabPanels,
+    Tab,
+    TabPanel,
+    Text,
+    Center,
+    VStack,
+    HStack,
+} from '@chakra-ui/react'
 import { MatchContainer, ByeContainer } from 'components'
 import EditButton from 'components/Dashboard/League/EditButton'
-import ErrorIcon from 'components/svg/ErrorIcon'
+import UpdatePlayerStatsIcon from 'components/svg/UpdatePlayerStatsIcon'
 
 // if using this for the non-logged in user pages, it might be good to add a
 // "showEditButtons" prop or something to hide the edit buttons
@@ -16,7 +26,11 @@ const RoundsView = ({ rounds }) => {
                     </Text>
                 </Center>
                 {rounds.map((_, i) => {
-                    return <Tab key={i} color="white" ml="0.4rem" w="1rem" h="2rem">{(i+1).toString()}</Tab>
+                    return (
+                        <Tab key={i} color="white" ml="0.4rem" w="1rem" h="2rem">
+                            {(i + 1).toString()}
+                        </Tab>
+                    )
                 })}
             </TabList>
             <TabPanels w="100%">
@@ -24,15 +38,23 @@ const RoundsView = ({ rounds }) => {
                     return (
                         <TabPanel paddingY="1rem" paddingX="0" w="100%" key={round._id}>
                             <VStack spacing="0.5rem" w="100%">
-                                {round.games.map((game) => 
+                                {round.games.map((game) => (
                                     <HStack key={game._id} w="100%">
                                         <MatchContainer game={game} />
-                                        {game.status == 'upcoming' ? <EditButton name={game._id} /> : <ErrorIcon width={48}/>} {/* TODO: replace me with proper icon */}
+                                        {game.status == 'upcoming' ? (
+                                            <EditButton name={game._id} />
+                                        ) : (
+                                            <EditButton
+                                                name={game._id}
+                                                icon={<UpdatePlayerStatsIcon />}
+                                                path={'stats'}
+                                            />
+                                        )}
                                     </HStack>
-                                )}
-                                {round.teamsOnBye.map((team) =>
-                                    <ByeContainer key={team._id} team={team}  />
-                                )}
+                                ))}
+                                {round.teamsOnBye.map((team) => (
+                                    <ByeContainer key={team._id} team={team} />
+                                ))}
                             </VStack>
                         </TabPanel>
                     )
