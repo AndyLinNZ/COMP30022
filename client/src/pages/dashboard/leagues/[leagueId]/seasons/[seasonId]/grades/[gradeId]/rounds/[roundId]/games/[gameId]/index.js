1c6b378a4be3d245a538681c92a2a45be8526737
Elisha Xin Yee Chung <elishakiera@gmail.com>
2021-10-11 14:10:00 +1100

    added isDesktop checking

--- client/src/pages/dashboard/leagues/[leagueId]/seasons/[seasonId]/grades/[gradeId]/rounds/[roundId]/games/[gameId]/index.js
+++ client/src/pages/dashboard/leagues/[leagueId]/seasons/[seasonId]/grades/[gradeId]/rounds/[roundId]/games/[gameId]/index.js
@@ -83,6 +83,135 @@ const index = () => {
         </Box>
     )
 
+    const GameDetails = ({season, grade, game}) => {
+
+        return (
+        <Box
+            h="100%"
+            w="100%"
+            bg="white"
+            borderRadius="1rem"
+            boxShadow="0 5px 10px rgba(0,0,0,0.1), 0 3px 3px rgba(0,0,0,0.12);"
+            padding="1rem"
+            overflowY="auto"
+            pos="relative"
+        >
+            <VStack spacing="1.25rem" h="100%">
+                <HStack w="full" spacing="0.5rem" >
+                    <Button 
+                        leftIcon={<ChevronLeftIcon />} 
+                        color="greyText.500"
+                        _hover={{ bg: "darkGrey" }} 
+                        variant="ghost" 
+                        fontSize="0.9rem"
+                        onClick={() =>
+                            router.back()
+                        }
+                    >
+                        BACK
+                    </Button>
+                    <Text color="greyBg" fontSize="1.1rem" lineHeight="1" pl={15}> {season?.name} </Text>
+                    <ChevronRightIcon
+                        w={[10, 12]}
+                        h={[10, 12]}
+                        color={'grey'}
+                    />
+                    <Text color="greyBg" fontSize="1.1rem" lineHeight="1"> {grade?.name} </Text>
+                    {hasStatus && 
+                        <Box pos="absolute" top="3" right="3" pt={2.5} pr={2.5}>
+                            <Tag type={game.status} text={game.status}/>
+                        </Box>
+                    }
+                </HStack>
+                <Divider orientation="horizontal" backgroundColor="gray.500" justifySelf="end" align="top"/>
+
+                {isDesktop ? <Grid templateColumns="3fr 1fr 0.5fr 1fr 3fr" gridGap="1rem" alignItems="center" justifyItems="center" w="100%" fontWeight="semibold" fontSize={isDesktop ? "3rem" : "1.5rem"}>
+                    <Text color="greyText.500" fontSize={isDesktop ? "3rem" : "1.5rem"}>{game?.team1?.team?.name}</Text>
+
+                    {hasResults ?
+                        <Text justifySelf="end" color="greyText.500" fontSize={isDesktop ? "1rem" : "0.5rem"}> {game?.team1.totalPoints} </Text>
+                        :
+                        <MinusIcon justifySelf="end" color="greyText.500"/>
+                    }
+
+                    <Divider orientation="vertical" backgroundColor="gray.500" justifySelf="center" align="top" fontSize={isDesktop ? "3rem" : "1.5rem"}/>
+
+                    {hasResults ?
+                        <Text justifySelf="start" color="greyText.500" fontSize={isDesktop ? "1rem" : "0.5rem"}> {game?.team2.totalPoints} </Text>
+                        :
+                        <MinusIcon justifySelf="start" color="greyText.500"/>
+                    }
+
+                    <Text color="greyText.500">{game?.team2?.team?.name}</Text>
+
+                </Grid> 
+                
+                :
+                <HStack>
+                <Grid templateRows="1fr 1fr" gridGap="1rem" alignItems="center" justifyItems="center" w="100%" fontWeight="semibold" fontSize={isDesktop ? "3rem" : "1.5rem"}>
+                    <Text justifyItems="center" color="greyText.500">{game?.team1?.team?.name}</Text>
+
+                    {hasResults ?
+                        <Text justifyItems="center" color="greyText.500"> 36 </Text>
+                        :
+                        <MinusIcon color="greyText.500"/>
+                    }
+
+                </Grid>
+                <Grid templateRows="1fr 1fr" gridGap="1rem" alignItems="center" justifyItems="center" w="100%" fontWeight="semibold" fontSize={isDesktop ? "3rem" : "1.5rem"}>
+                    <Text justifyItems="center" color="greyText.500">{game?.team2?.team?.name}</Text>
+
+                    {hasResults ?
+                        <Text justifyItems="center" color="greyText.500"> 125 </Text>
+                        :
+                        <MinusIcon color="greyText.500"/>
+                    }
+
+                </Grid>
+                </HStack>
+                
+
+
+                }
+                <Divider orientation="horizontal" backgroundColor="gray.500" justifySelf="end" align="top"/>
+
+
+                <Grid templateColumns="1fr 1fr" gridGap="1rem" alignItems="center" w="100%" justifyItems="center">
+                    <HStack>
+                        <TimeIcon color="greyText.500"/>
+                        <Text color="greyText.500">From {new Date(game?.dateStart).toLocaleString()} to {new Date(game?.dateFinish).toLocaleString()} </Text>
+                    </HStack>
+
+                    <HStack>
+                        <StarIcon color="greyText.500"/>
+                        <Text color="greyText.500">{game?.locationName}</Text>
+                    </HStack>
+
+                </Grid>
+            </VStack>
+        </Box>
+        )
+    }
+
+    const BoxScore = ({game}) => {
+
+        return isDesktop ? (
+            <HStack w="full" spacing="0.5rem" >      
+                {hasResults && 
+                    <PlayersTable playersStats={game.team1.playersStats}/>}
+                {hasResults && 
+                    <PlayersTable playersStats={game.team2.playersStats}/>}
+            </HStack>
+        ) : 
+        <VStack w="full" spacing="0.5rem" >      
+            {hasResults && 
+                <PlayersTable playersStats={game.team1.playersStats}/>}
+            {hasResults && 
+                <PlayersTable playersStats={game.team2.playersStats}/>}
+        </VStack>
+
+    }
+
     return (
         <Template>
             <VStack
@@ -107,95 +236,8 @@ const index = () => {
                         {league?.organisation}
                     </Box>
                 </VStack>
-                <Box
-                    h="100%"
-                    w="100%"
-                    bg="white"
-                    borderRadius="1rem"
-                    boxShadow="0 5px 10px rgba(0,0,0,0.1), 0 3px 3px rgba(0,0,0,0.12);"
-                    padding="1rem"
-                    overflowY="auto"
-                    pos="relative"
-                >
-                    <VStack spacing="1.25rem" h="100%">
-                        <HStack w="full" spacing="0.5rem" >
-                            <Box>
-                                <Button 
-                                    leftIcon={<ChevronLeftIcon />} 
-                                    color="greyText.500"
-                                    _hover={{ bg: "darkGrey" }} 
-                                    variant="ghost" 
-                                    fontSize="0.9rem"
-                                    onClick={() =>
-                                        router.back()
-                                    }
-                                >
-                                    BACK
-                                </Button>
-                            </Box>
-                            <Box w="20%" fontSize="1.1rem" lineHeight="1" pl={15} color="greyBg">
-                                {season?.name}
-                            </Box>
-                            <ChevronRightIcon
-                                w={[10, 12]}
-                                h={[10, 12]}
-                                color={'grey'}
-                            />
-                            <Box w="20%" fontSize="1.1rem" lineHeight="1" color="greyBg">
-                                {grade?.name}
-                            </Box>
-                            {hasStatus && 
-                                <Box pos="absolute" top="3" right="3" pt={2.5} pr={2.5}>
-                                    <Tag type={game.status} text={game.status}/>
-                                </Box>
-                            }
-                        </HStack>
-                        <Divider orientation="horizontal" backgroundColor="gray.500" justifySelf="end" align="top"/>
-
-
-                        <Grid templateColumns="3fr 1fr 0.5fr 1fr 3fr" gridGap="1rem" alignItems="center" w="100%" fontWeight="semibold" fontSize="3rem">
-                            <Text justifySelf="center" color="greyText.500">{game?.team1?.team?.name}</Text>
-
-                            {hasResults ?
-                                <Text justifySelf="end" color="greyText.500"> {game?.team1.totalPoints} </Text>
-                                :
-                                <MinusIcon justifySelf="end" color="greyText.500"/>
-                            }
-
-                            <Divider orientation="vertical" backgroundColor="gray.500" justifySelf="center" align="top"/>
-
-                            {hasResults ?
-                                <Text justifySelf="start" color="greyText.500"> {game?.team2.totalPoints} </Text>
-                                :
-                                <MinusIcon justifySelf="start" color="greyText.500"/>
-                            }
-
-                            <Text justifySelf="center" color="greyText.500">{game?.team2?.team?.name}</Text>
-
-                        </Grid>
-                        <Divider orientation="horizontal" backgroundColor="gray.500" justifySelf="end" align="top"/>
-
-
-                        <Grid templateColumns="1fr 1fr" gridGap="1rem" alignItems="center" w="100%" justifyItems="center">
-                            <HStack>
-                                <TimeIcon color="greyText.500"/>
-                                <Text color="greyText.500">From {new Date(game?.dateStart).toLocaleString()} to {new Date(game?.dateFinish).toLocaleString()} </Text>
-                            </HStack>
-
-                            <HStack>
-                                <StarIcon color="greyText.500"/>
-                                <Text color="greyText.500">{game?.locationName}</Text>
-                            </HStack>
-
-                        </Grid>
-                    </VStack>
-                </Box>
-                <HStack w="full" spacing="0.5rem" >      
-                    {hasResults && 
-                        <PlayersTable playersStats={game.team1.playersStats}/>}
-                    {hasResults && 
-                        <PlayersTable playersStats={game.team2.playersStats}/>}
-                </HStack>
+                <GameDetails season={season} grade={grade} game={game}/>
+                <BoxScore game={game}/>
             </VStack>
         </Template>
     )
