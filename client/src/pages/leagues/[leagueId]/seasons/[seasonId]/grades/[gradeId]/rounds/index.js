353dfc9d8a17d3df040a76c58278cc5d5b351f8f
Andy Lin <andylinnz79@gmail.com>
2021-10-14 20:07:54 +1100

    non-user team pages

--- client/src/pages/leagues/[leagueId]/seasons/[seasonId]/grades/[gradeId]/rounds/index.js
+++ client/src/pages/leagues/[leagueId]/seasons/[seasonId]/grades/[gradeId]/rounds/index.js
@@ -7,7 +7,7 @@ import { VStack, HStack, Button, Text } from '@chakra-ui/react'
 import { Container, RoundsView } from 'components'
 
 const index = () => {
-    const { grade } = useGrade()
+    const { grade, isLoading } = useGrade()
     const { league } = useLeague()
     const router = useRouter()
 
@@ -21,27 +21,43 @@ const index = () => {
             <Container league={league}>
                 <VStack spacing="1.25rem" h="100%">
                     <HStack spacing="1rem">
-                        <Button bg="greyText.500" color="white">FIXTURE</Button>
-                        <Button
-                            onClick={() => router.push(window.location.pathname + '/ladder')}
-                        >LADDER</Button>
+                        <Button bg="greyText.500" color="white">
+                            FIXTURE
+                        </Button>
+                        <Button onClick={() => router.push(window.location.pathname + '/ladder')}>
+                            LADDER
+                        </Button>
                     </HStack>
-
-                    {hasRounds ?
-                        <RoundsView rounds={grade.fixture} showEditButtons={false} />
-                        :
-                        <Text
-                            fontSize="2rem"
-                            textAlign="center"
-                            color="greyText"
-                            pos="absolute"
+                    {isLoading ? (
+                        <Spinner
+                            thickness="4px"
+                            speed="0.65s"
+                            emptyColor="gray.200"
+                            color="blue.500"
+                            size="xl"
+                            position="absolute"
                             top="50%"
-                            left="50%"
-                            transform="translate(-50%, -50%)"
-                        >
-                            This Grade does not have a fixture yet!
-                        </Text>
-                    }
+                            left="calc(50% - 1rem)"
+                        />
+                    ) : (
+                        <>
+                            {hasRounds ? (
+                                <RoundsView rounds={grade.fixture} showEditButtons={false} />
+                            ) : (
+                                <Text
+                                    fontSize="2rem"
+                                    textAlign="center"
+                                    color="greyText"
+                                    pos="absolute"
+                                    top="50%"
+                                    left="50%"
+                                    transform="translate(-50%, -50%)"
+                                >
+                                    This Grade does not have a fixture yet!
+                                </Text>
+                            )}
+                        </>
+                    )}
                 </VStack>
             </Container>
         </Template>
