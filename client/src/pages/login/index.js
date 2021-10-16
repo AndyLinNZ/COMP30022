da35102a54b916b16cf8f09a536f75763dc177cd
Joseph Surin <joseph.surin@gmail.com>
2021-10-16 11:14:58 +1100

    redirect suggestions

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 import LoginForm from 'components/LoginPage/LoginForm'
 import { Box, VStack } from '@chakra-ui/react'
 import Logo from 'components/svg/Logo'
+import { appPaths } from 'utils/constants'
 
 const index = () => {
+    const router = useRouter()
+
     return (
         <Box
             w="100vw"
@@ -19,7 +23,11 @@ const index = () => {
                 <title>Dribblr | Log in</title>
             </Head>
             <VStack spacing="1rem">
-                <Logo width="300" />
+                <Logo
+                    width="300"
+                    cursor="pointer"
+                    onClick={() => router.push(appPaths.HOME_PATH)}
+                />
                 <LoginForm />
             </VStack>
         </Box>
--- client/src/pages/register/index.js
+++ client/src/pages/register/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 import RegisterForm from 'components/RegisterPage/RegisterForm'
 import { Box, VStack } from '@chakra-ui/react'
 import Logo from 'components/svg/Logo'
+import { appPaths } from 'utils/constants'
 
 const index = () => {
+    const router = useRouter()
+
     return (
         <Box
             w="100vw"
@@ -19,7 +23,11 @@ const index = () => {
                 <title>Dribblr | Sign up</title>
             </Head>
             <VStack spacing="1rem">
-                <Logo width="300" />
+                <Logo
+                    width="300"
+                    cursor="pointer"
+                    onClick={() => router.push(appPaths.HOME_PATH)}
+                />
                 <RegisterForm />
             </VStack>
         </Box>
